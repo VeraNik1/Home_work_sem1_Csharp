7620c08aa519304419e180e99c6f242272de39e4
Vera <solnce288888@gmail.com>
2022-09-30 22:35:20 +0300

    home work num 6 added

--- Program.cs
+++ Program.cs
@@ -38,3 +38,18 @@ if(c > max){
 }
 Console.WriteLine($"Максимальное число {max}");
 
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Введите число");
+int Number = int.Parse(Console.ReadLine()!);
+if(Number % 2 == 0 && Number != 0) // 0 не является четным
+{
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
